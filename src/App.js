8d75afa2d62deb971fd3a0dca95ee01a230d1274
pigji <wjlqlrll@naver.com>
2024-12-24 15:42:10 +0900

    12.24

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import {Routes, Route} from 'react-router-dom';
+import './App.scss';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';  //링크태그
+import { RiMenu3Fill } from "react-icons/ri"; //아이콘
+import { IoCloseSharp } from "react-icons/io5"; //아이콘
+import $ from 'jquery'; //리액트 제이쿼리
+import { Color } from 'antd/es/color-picker';
+
+const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(true);
+  const activeStyle = {
+    color : 'cadetblue'
+  }
+  const toggleMenu = () => {
+    setMenuOpen(prevMenuOpen => !prevMenuOpen);
+    $('.mobile-header-drop').slideToggle(500)
+  }
+
+  return (
+    <div className='header'>
+      <div className="header-inner">
+        <div className="logo">
+          <Link to="/" >
+            <img src={process.env.PUBLIC_URL + "/images/logo.png"} alt="logo" />
+          </Link>
+        </div>
+        <div className="nav">
+          <ul className='loginAndSignup'>
+            <li><Link to="/" ></Link>Login</li>
+            <li><Link to="/" ></Link>SignUp</li>
+            <li><Link to="/" ></Link>Home</li>
+            <li><Link to="/" ></Link>About</li>
+          </ul>
+          {/* <ul className='menu'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+          </ul> */}
+        </div>
+        <div className="movieNav">
+          <span>
+            {
+              menuOpen ? (<RiMenu3Fill onClick={toggleMenu} />) : (<IoCloseSharp onClick={toggleMenu } />)
+            }
+          </span>
+          <div className={`mobile-header-drop ${menuOpen ? '' : 'open'}`}>
+            <div>
+              <div className="mobile-header-nav">
+                <div className="mobile-header-menu">
+                  <Link to="/" className='logoLink' >
+                    <img src={process.env.PUBLIC_URL + "/images/logo.png"} alt="logo" />
+                  </Link>
+                  <ul>
+                    <li>
+                      <NavLink to="/" style={({isActive}) => (isActive ? activeStyle:undefined)}>Home</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/about" style={({isActive}) => (isActive ? activeStyle:undefined)}>About</NavLink>
+                    </li>
+                  </ul>
+                </div>
+                <div className="mobile-header-loginJoin">
+                  <ul>
+                    <li>
+                      <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                      <Link Link to="/signup">SignUp</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
